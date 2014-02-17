c56dd72a8c81f211c13a87926aa780ec4aad77ad
Mikael Brevik <mikaelbre@gmail.com>
2014-02-17 18:01:12 +0100

    Cleans up in tests, organizing by functionality.

--- test/main.js
+++ test/main.js
@@ -22,12 +22,12 @@ var originalLogger = notify.logger();
 
 describe('gulp output stream', function() {
 
-  describe('notify()', function() {
+  beforeEach(function () {
+    notify.setLogLevel(0);
+    notify.logger(originalLogger);
+  });
 
-    beforeEach(function () {
-      notify.setLogLevel(0);
-      notify.logger(originalLogger);
-    });
+  describe('notify()', function() {
 
     it('should return a stream', function(done) {
       var stream = notify({
@@ -48,54 +48,6 @@ describe('gulp output stream', function() {
       done();
     });
 
-    it('should allow setting of logLevel', function(done) {
-      var notifier = notify.withReporter(mockGenerator);
-      should.exist(notify.setLogLevel);
-      should.exist(notify.logger);
-      done();
-    });
-
-    it('should log error on log level 1', function(done) {
-      var srcFile = join(__dirname, "./fixtures/1.txt");
-      notify.setLogLevel(1);
-      notify.logger(function (options) {
-        should.exist(true);
-        done();
-      });
-      var notifier = notify.withReporter(mockGenerator);
-
-      gulp.src(srcFile)
-        .pipe(through.obj(function (file, enc, cb) {
-          this.emit("error", new gutil.PluginError("testPlugin", "foo"));
-          cb();
-        }))
-        .on("error", notifier.onError())
-
-    });
-
-    it('should not log on log level 0', function(done) {
-      var srcFile = join(__dirname, "./fixtures/1.txt");
-      var hasBeenCalled = false;
-      notify.setLogLevel(0);
-
-      notify.logger(function () {
-        hasBeenCalled = true;
-        should.not.exist(arguments);
-      });
-
-      var notifier = notify.withReporter(mockGenerator(function () {
-        should(hasBeenCalled).equal(false);
-        done();
-      }));
-
-      gulp.src(srcFile)
-        .pipe(through.obj(function (file, enc, cb) {
-          this.emit("error", new gutil.PluginError("testPlugin", "foo"));
-          cb();
-        }))
-        .on("error", notifier.onError())
-    });
-
     it('should call notifier with title and message', function(done) {
       var testString = "this is a test";
 
@@ -105,7 +57,6 @@ describe('gulp output stream', function() {
         should.exist(opts.message);
         String(opts.message).should.equal(testString);
       }));
-
 
       var instream = gulp.src(join(__dirname, "./fixtures/*.txt")),
           outstream = mockedNotify({
@@ -216,7 +167,6 @@ describe('gulp output stream', function() {
           done();
         }));
     });
-
 
     it('should emit error when sub-module throws exception/error', function(done) {
       var testString = "some exception",
@@ -329,127 +279,6 @@ describe('gulp output stream', function() {
       instream.pipe(outstream)
     });
 
-
-    it('should only notify on the last file, if onLast flag is activated', function(done) {
-      var
-        testSuffix = "tester",
-        srcFile = join(__dirname, "./fixtures/*"),
-        instream = gulp.src(srcFile),
-        numFunctionCalls = 0,
-        outstream = notify({
-          onLast: true,
-          notifier: mockGenerator(function (opts) {
-            should.exist(opts);
-            should.exist(opts.title);
-            should.exist(opts.message);
-            numFunctionCalls++;
-          })
-        });
-
-      outstream.on('data', function(file) {
-        should.exist(file);
-        should.exist(file.path);
-        should.exist(file.contents);
-      });
-
-      outstream.on('end', function() {
-        numFunctionCalls.should.equal(1);
-        done();
-      });
-
-      instream.pipe(outstream);
-    });
-
-
-    it('should stream all files even if onLast is activated', function(done) {
-      var
-        testSuffix = "tester",
-        srcFile = join(__dirname, "./fixtures/*"),
-        instream = gulp.src(srcFile),
-        outstream = notify({
-          onLast: true,
-          notifier: mockGenerator()
-        });
-
-      var numFilesBefore = 0,
-          numFilesAfter = 0;
-
-      instream
-        .pipe(through.obj(function (file, enc, cb) {
-          numFilesBefore++;
-          this.push(file);
-          cb();
-        }, function (cb) {
-          numFilesBefore.should.equal(3);
-          cb();
-        }))
-        .pipe(outstream)
-        .pipe(through.obj(function (file, enc, cb) {
-          numFilesAfter++;
-          this.push(file);
-          cb();
-        }, function (callback) {
-          numFilesAfter.should.equal(3);
-          done();
-        }));
-    });
-
-    it('should have defined onError function on object', function (done) {
-      should.exist(notify.onError);
-      done();
-    });
-
-    it('should have onError event withReporter', function(done) {
-      var notifier = notify.withReporter(mockGenerator);
-      should.exist(notifier.onError);
-      done();
-    });
-
-    it('should be limited by notifying on error if th onError-option is passed', function (done) {
-      var
-        testMessage = "tester",
-        srcFile = join(__dirname, "./fixtures/*"),
-        onError = notify.onError({
-          notifier: mockGenerator(function (opts) {
-            should.exist(opts);
-            should.exist(opts.title);
-            should.exist(opts.message);
-            String(opts.message).should.equal(testMessage);
-            String(opts.title).should.equal("Error running Gulp");
-            done();
-          })
-        });
-
-      gulp.src(srcFile)
-        .pipe(through.obj(function (file, enc, cb) {
-          this.emit("error", new gutil.PluginError("testPlugin", testMessage));
-          cb();
-        }))
-        .on("error", onError)
-    });
-
-    it('should have onError options on withReporter sugar', function (done) {
-
-      var custom = notify.withReporter(mockGenerator(function (opts) {
-            should.exist(opts);
-            should.exist(opts.title);
-            should.exist(opts.message);
-            String(opts.message).should.equal(testMessage);
-            String(opts.title).should.equal("Error running Gulp");
-            done();
-          }));
-      var
-        testMessage = "tester",
-        srcFile = join(__dirname, "./fixtures/*");
-
-      gulp.src(srcFile)
-        .pipe(through.obj(function (file, enc, cb) {
-          this.emit("error", new gutil.PluginError("testPlugin", testMessage));
-          cb();
-        }))
-        .on("error", custom.onError())
-    });
-
     it('should handle streamed files', function (done) {
       var expectedFile = new gutil.File({
         path: "test/fixtures/1.txt",
@@ -522,29 +351,71 @@ describe('gulp output stream', function() {
       outstream.write(expectedFile);
     });
 
-    it('should support lodash template for titles and messages on onError', function (done) {
-      var testString = "Template: <%= error.message %>";
-      var expectedString = "Template: test";
-      var srcFile = join(__dirname, "./fixtures/*");
-      var onError = notify.onError({
-            message: testString,
-            title: testString,
-            notifier: mockGenerator(function (opts) {
-              should.exist(opts);
-              should.exist(opts.title);
-              should.exist(opts.message);
-              String(opts.message).should.equal(expectedString);
-              String(opts.title).should.equal(expectedString);
-              done();
-            })
-          });
+  });
 
-      gulp.src(srcFile)
+  describe('notify.onLast', function() {
+
+    it('should only notify on the last file, if onLast flag is activated', function(done) {
+      var
+        testSuffix = "tester",
+        srcFile = join(__dirname, "./fixtures/*"),
+        instream = gulp.src(srcFile),
+        numFunctionCalls = 0,
+        outstream = notify({
+          onLast: true,
+          notifier: mockGenerator(function (opts) {
+            should.exist(opts);
+            should.exist(opts.title);
+            should.exist(opts.message);
+            numFunctionCalls++;
+          })
+        });
+
+      outstream.on('data', function(file) {
+        should.exist(file);
+        should.exist(file.path);
+        should.exist(file.contents);
+      });
+
+      outstream.on('end', function() {
+        numFunctionCalls.should.equal(1);
+        done();
+      });
+
+      instream.pipe(outstream);
+    });
+
+    it('should stream all files even if onLast is activated', function(done) {
+      var
+        testSuffix = "tester",
+        srcFile = join(__dirname, "./fixtures/*"),
+        instream = gulp.src(srcFile),
+        outstream = notify({
+          onLast: true,
+          notifier: mockGenerator()
+        });
+
+      var numFilesBefore = 0,
+          numFilesAfter = 0;
+
+      instream
         .pipe(through.obj(function (file, enc, cb) {
-          this.emit("error", new gutil.PluginError("testPlugin", "test"));
+          numFilesBefore++;
+          this.push(file);
+          cb();
+        }, function (cb) {
+          numFilesBefore.should.equal(3);
           cb();
         }))
-        .on('error', onError);
+        .pipe(outstream)
+        .pipe(through.obj(function (file, enc, cb) {
+          numFilesAfter++;
+          this.push(file);
+          cb();
+        }, function (callback) {
+          numFilesAfter.should.equal(3);
+          done();
+        }));
     });
 
     it('should support lodash template for titles and messages when onLast', function (done) {
@@ -578,6 +449,146 @@ describe('gulp output stream', function() {
       });
 
       instream.pipe(outstream);
+    });
+
+  });
+
+
+  describe('notify.logger()', function() {
+
+    it('should allow setting of logLevel', function(done) {
+      var notifier = notify.withReporter(mockGenerator);
+      should.exist(notify.setLogLevel);
+      should.exist(notify.logger);
+      done();
+    });
+
+    it('should log error on log level 1', function(done) {
+      var srcFile = join(__dirname, "./fixtures/1.txt");
+      notify.setLogLevel(1);
+      notify.logger(function (options) {
+        should.exist(true);
+        done();
+      });
+      var notifier = notify.withReporter(mockGenerator);
+
+      gulp.src(srcFile)
+        .pipe(through.obj(function (file, enc, cb) {
+          this.emit("error", new gutil.PluginError("testPlugin", "foo"));
+          cb();
+        }))
+        .on("error", notifier.onError())
+
+    });
+
+    it('should not log on log level 0', function(done) {
+      var srcFile = join(__dirname, "./fixtures/1.txt");
+      var hasBeenCalled = false;
+      notify.setLogLevel(0);
+
+      notify.logger(function () {
+        hasBeenCalled = true;
+        should.not.exist(arguments);
+      });
+
+      var notifier = notify.withReporter(mockGenerator(function () {
+        should(hasBeenCalled).equal(false);
+        done();
+      }));
+
+      gulp.src(srcFile)
+        .pipe(through.obj(function (file, enc, cb) {
+          this.emit("error", new gutil.PluginError("testPlugin", "foo"));
+          cb();
+        }))
+        .on("error", notifier.onError())
+    });
+  });
+
+  describe('notify.onError()', function() {
+
+    it('should have defined onError function on object', function (done) {
+      should.exist(notify.onError);
+      done();
+    });
+
+    it('should have onError event withReporter', function(done) {
+      var notifier = notify.withReporter(mockGenerator);
+      should.exist(notifier.onError);
+      done();
+    });
+
+
+    it('should be limited by notifying on error if th onError-option is passed', function (done) {
+      var
+        testMessage = "tester",
+        srcFile = join(__dirname, "./fixtures/*"),
+        onError = notify.onError({
+          notifier: mockGenerator(function (opts) {
+            should.exist(opts);
+            should.exist(opts.title);
+            should.exist(opts.message);
+            String(opts.message).should.equal(testMessage);
+            String(opts.title).should.equal("Error running Gulp");
+            done();
+          })
+        });
+
+      gulp.src(srcFile)
+        .pipe(through.obj(function (file, enc, cb) {
+          this.emit("error", new gutil.PluginError("testPlugin", testMessage));
+          cb();
+        }))
+        .on("error", onError)
+    });
+
+
+
+    it('should have onError options on withReporter sugar', function (done) {
+
+      var custom = notify.withReporter(mockGenerator(function (opts) {
+            should.exist(opts);
+            should.exist(opts.title);
+            should.exist(opts.message);
+            String(opts.message).should.equal(testMessage);
+            String(opts.title).should.equal("Error running Gulp");
+            done();
+          }));
+      var
+        testMessage = "tester",
+        srcFile = join(__dirname, "./fixtures/*");
+
+      gulp.src(srcFile)
+        .pipe(through.obj(function (file, enc, cb) {
+          this.emit("error", new gutil.PluginError("testPlugin", testMessage));
+          cb();
+        }))
+        .on("error", custom.onError())
+    });
+
+    it('should support lodash template for titles and messages on onError', function (done) {
+      var testString = "Template: <%= error.message %>";
+      var expectedString = "Template: test";
+      var srcFile = join(__dirname, "./fixtures/*");
+      var onError = notify.onError({
+            message: testString,
+            title: testString,
+            notifier: mockGenerator(function (opts) {
+              should.exist(opts);
+              should.exist(opts.title);
+              should.exist(opts.message);
+              String(opts.message).should.equal(expectedString);
+              String(opts.title).should.equal(expectedString);
+              done();
+            })
+          });
+
+      gulp.src(srcFile)
+        .pipe(through.obj(function (file, enc, cb) {
+          this.emit("error", new gutil.PluginError("testPlugin", "test"));
+          cb();
+        }))
+        .on('error', onError);
     });
   });
 });
